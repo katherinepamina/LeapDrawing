eaa75d1d61041cf723c0187bdef6df15f897400d
shulerhl <hollidayshuler2016@u.northwestern.edu>
2016-04-20 13:36:35 -0500

    Working without drawing anything

--- test.js
+++ test.js
@@ -29,6 +29,36 @@ var controllerOptions = {enableGestures: true};
 
 // to use HMD mode:
 // controllerOptions.optimizeHMD = true;
+  var canvas = document.getElementById('webgl');
+
+  // Get the rendering context for WebGL
+  var gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+  }
+
+  // Initialize shaders
+  if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+    console.log('Failed to intialize shaders.');
+  }
+
+  // Write the positions of vertices to a vertex shader
+  var n = initVertexBuffers(gl);
+  if (n < 0) {
+    console.log('Failed to set the positions of the vertices');
+  }
+
+  // Specify the color for clearing <canvas>
+  gl.clearColor(0, 0, 0, 1);
+
+  // Get storage location of u_ModelMatrix
+  var u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+  if (!u_ModelMatrix) { 
+    console.log('Failed to get the storage location of u_ModelMatrix');
+  }
+
+  // Model matrix
+  var modelMatrix = new Matrix4();
 
 Leap.loop(controllerOptions, function(frame) {
   if (paused) {
@@ -231,4 +261,51 @@ function pauseForGestures() {
   } else {
     pauseOnGesture = false;
   }
+}
+
+function initVertexBuffers(gl) {
+  var vertices = new Float32Array ([
+    0, 0.5,   -0.5, -0.5,   0.5, -0.5
+  ]);
+  var n = 3;   // The number of vertices
+
+  // Create a buffer object
+  var vertexBuffer = gl.createBuffer();
+  if (!vertexBuffer) {
+    console.log('Failed to create the buffer object');
+    return -1;
+  }
+
+  // Bind the buffer object to target
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  // Write date into the buffer object
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+  // Assign the buffer object to a_Position variable
+  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  if(a_Position < 0) {
+    console.log('Failed to get the storage location of a_Position');
+    return -1;
+  }
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+  // Enable the assignment to a_Position variable
+  gl.enableVertexAttribArray(a_Position);
+
+  return n;
+}
+
+function draw(gl, n, currentAngle, modelMatrix, u_ModelMatrix) {
+  // Set the rotation matrix
+  modelMatrix.setRotate(currentAngle, 0, 0, 1);
+  modelMatrix.translate(0.35, 0, 0);
+ 
+  // Pass the rotation matrix to the vertex shader
+  gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  // Draw the rectangle
+  gl.drawArrays(gl.TRIANGLES, 0, n);
 }

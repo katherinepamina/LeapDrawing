842c28df159d4a5ee45ea907e52c68b05999ed47
shulerhl <hollidayshuler2016@u.northwestern.edu>
2016-05-04 10:47:07 -0500

    changed cat object to be called Asteroid

--- space.js
+++ space.js
@@ -1,5 +1,5 @@
 var ships = {};
-var cats = {};
+var asteroids = {};
 var shots = [];
 var output = document.getElementById('crash');
 var score = document.getElementById('score');
@@ -31,9 +31,9 @@ Leap.loop(function(frame) {
       }
         
       for(var i = 0; i < 25; i++){
-        var cat = (cats[i] || (cats[i] = new Cat()));
+        var asteroid = (asteroids[i] || (asteroids[i] = new Asteroid()));
         var pos = hand.screenPosition();
-        if(Math.abs(cat.position[0]-pos[0]) < cat.siz/2 && Math.abs(cat.position[1]-pos[1]) < cat.siz/2){
+        if(Math.abs(asteroid.position[0]-pos[0]) < asteroid.siz/2 && Math.abs(asteroid.position[1]-pos[1]) < asteroid.siz/2){
           output.innerHTML = "Crash!";
           crashed = true;
           crash(pos);
@@ -41,19 +41,19 @@ Leap.loop(function(frame) {
         }
         else {
           for(var j = 0; j < shots.length; j++){
-            if(Math.abs(cat.position[0]-shots[j].position[0]) < cat.siz/2 && Math.abs(cat.position[1]-shots[j].position[1]) < cat.siz/2)
+            if(Math.abs(asteroid.position[0]-shots[j].position[0]) < asteroid.siz/2 && Math.abs(asteroid.position[1]-shots[j].position[1]) < asteroid.siz/2)
             {
               count += 1000;
-              cat.position[0] = Math.random()*window.innerWidth;
-              cat.position[1] = Math.random()*10;
-              cat.siz = 10;
+              asteroid.position[0] = Math.random()*window.innerWidth;
+              asteroid.position[1] = Math.random()*10;
+              asteroid.siz = 10;
             }
           }
         }
-          cat.position[0] += 5;
-          cat.position[1] += 5;
-          cat.siz += 1;
-          cat.setTransform([cat.position[0], cat.position[1]], cat.siz);
+          asteroid.position[0] += 5;
+          asteroid.position[1] += 5;
+          asteroid.siz += 1;
+          asteroid.setTransform([asteroid.position[0], asteroid.position[1]], asteroid.siz);
         now = Date.now();
         count = (now - last);
         
@@ -64,17 +64,17 @@ Leap.loop(function(frame) {
 
 
 var SpaceShip = function() {
-  var cat = this;
+  var ship = this;
   var img = document.createElement('img');
   img.src = 'http://www.clker.com/cliparts/V/5/Q/z/q/m/fighter-jet-hi.png';
   img.style.position = 'absolute';
   img.style.height = '10%';
   img.onload = function () {
-    cat.setTransform([window.innerWidth/2,window.innerHeight/2], 0);
+    ship.setTransform([window.innerWidth/2,window.innerHeight/2], 0);
     document.body.appendChild(img);
   }
 
-  cat.setTransform = function(position, rotation) {
+  ship.setTransform = function(position, rotation) {
 
     img.style.left = position[0] - img.width  / 2 + 'px';
     img.style.top  = position[1] - img.height / 2 + 'px';
@@ -88,8 +88,8 @@ var SpaceShip = function() {
 
 };
 
-var Cat = function() {
-  var cat = this;
+var Asteroid = function() {
+  var asteroid = this;
   this.position = [Math.random()*window.innerWidth, Math.random()*10];
   this.siz = 10;
   var img = document.createElement('img');
@@ -98,11 +98,11 @@ var Cat = function() {
   img.style.height = "10px";
   img.style.width = "10px";
   img.onload = function () {
-    cat.setTransform([window.innerWidth/2,window.innerHeight/2], 0);
+    asteroid.setTransform([window.innerWidth/2,window.innerHeight/2], 0);
     document.body.appendChild(img);
   }
 
-  cat.setTransform = function(position, siz) {
+  asteroid.setTransform = function(position, siz) {
     if(position[0] >= window.innerWidth || position[1] >= window.innerHeight || siz > 125){
       this.position[0] = Math.random()*window.innerWidth;
       this.position[1] = Math.random()*10;
